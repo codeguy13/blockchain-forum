4581494eb50790c8b67d97c6df4d520bcd67f220
codeguy13 <bensilverstein@protonmail.com>
2021-05-13 05:58:58 +0000

    Fixed some packaging issues

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import find_packages, setup
+import os
 
 setup(name='blockforum',
       version='0.0.1',
-      py_modules=['blockchain', 'p2p'],
+      py_modules=['blockchain', './blockchain/interface', 'p2p'],
       packages=find_packages(),
       zip_safe=False,
       include_package_data=True,
